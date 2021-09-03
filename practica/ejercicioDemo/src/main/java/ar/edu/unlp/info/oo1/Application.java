d6bbc505e959362f4715f592246eb7fdb598ffa8
Ramiro González Maciel <ramirogm@gmail.com>
2021-09-03 05:42:17 -0300

    Agrega ejercicioDemo

---
+++ practica/ejercicioDemo/src/main/java/ar/edu/unlp/info/oo1/Application.java
@@ -0,0 +1,9 @@
+package ar.edu.unlp.info.oo1;
+
+public class Application {
+
+  public static void main(String[] args) {
+    
+    System.out.println("Hola Mundo!");
+  }
+}
---
+++ practica/ejercicioDemo/src/test/java/ar/edu/unlp/info/oo1/PersonaTest.java
@@ -0,0 +1,27 @@
+package ar.edu.unlp.info.oo1;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Aca escribiremos los test de unidad para cada clase 
+ * 
+ */
+public class PersonaTest {
+	
+	Persona james;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		james = new Persona();
+		james.setApellido("Gosling");
+		james.setNombre("James");
+	}
+	
+    @Test
+    public void testNombreCompleto() {
+        assertEquals("Gosling, James", james.getNombreCompleto());
+    }
+}
